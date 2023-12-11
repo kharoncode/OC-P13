b7b92ca439d8e8a9d9b4dd220780337449ab524c
Remi Collin <sharon.code@proton.me>
2023-12-11 15:56:23 +0100

    import design for sign-in

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from '@components/footer/Footer';
 import Header from '@components/header/Header';
 import Home from '@pages/home/Home';
+import SignIn from '@pages/signIn/SignIn';
 
 function App() {
    return (
@@ -9,6 +10,7 @@ function App() {
          <Header />
          <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
          </Routes>
          <Footer />
       </Router>
